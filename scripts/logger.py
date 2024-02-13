a1112250f2e2427f72533aae6ca89674270a77b0
Marvellous <akomolafemarvellous@gmail.com>
2024-02-13 13:45:43 +0100

    Added the logger file

---
+++ scripts/logger.py
@@ -0,0 +1,22 @@
+# Path: scripts/logger.py
+
+# Import the required libraries
+import logging
+from logging.handlers import RotatingFileHandler
+
+   
+def logs(__name__):
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)  # Set the logging level for this logger
+
+    # Create a rotating file handler
+    handler = RotatingFileHandler('./logs/app.log', maxBytes=10000000, backupCount=5)
+
+    # Create a formatter and add it to the handler
+    formatter = logging.Formatter('%(asctime)s - %(name)s - [%(levelname)s] - %(message)s')
+    handler.setFormatter(formatter)
+
+    # Add the handler to the logger
+    logger.addHandler(handler)
+    
+    return logger
