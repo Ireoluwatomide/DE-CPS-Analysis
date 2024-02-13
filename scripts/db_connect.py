b8754866709d655dc2049d0f5b273845c53032e7
Marvellous <akomolafemarvellous@gmail.com>
2024-02-13 13:42:45 +0100

    Added the db object file

---
+++ scripts/db_connect.py
@@ -0,0 +1,52 @@
+# Path: scripts/db_connect.py      
+
+# Import the required libraries
+import os
+import psycopg2
+from .logger import logs
+from psycopg2 import OperationalError
+
+
+class Database:
+    def __init__(self):
+        self.conn = None
+        self.cur = None
+        self.logger = logs(__name__)
+
+    def connect(self):
+        try:
+            self.conn = psycopg2.connect(
+                dbname=os.environ.get("DB_NAME"),
+                user=os.environ.get("DB_USER"),
+                password=os.environ.get("DB_PASSWORD"),
+                host=os.environ.get("DB_HOST"),
+                port=os.environ.get("DB_PORT")
+            )
+            self.cur = self.conn.cursor()
+            self.logger.info("Connected to the database.")
+        except OperationalError as e:
+            self.logger.error(f"Error: {e}")
+
+    def disconnect(self):
+        try:
+            if self.cur:
+                self.cur.close()
+            if self.conn:
+                self.conn.close()
+            self.logger.info("Disconnected from the database.")
+        except Exception as e:
+            self.logger.error(f"Error: {e}")
+            
+    def execute(self, query):
+        try:
+            self.cur.execute(query)
+            self.logger.info("Query executed successfully.")
+        except Exception as e:
+            self.logger.error(f"Error executing query: {e}")
+
+    def commit(self):
+        try:
+            self.conn.commit()
+            self.logger.info("Changes committed successfully.")
+        except Exception as e:
+            self.logger.error(f"Error committing changes: {e}")
