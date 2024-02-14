b72abd341215b1606beca1e65d5f1eec837d29a1
Marvellous <akomolafemarvellous@gmail.com>
2024-02-14 16:54:06 +0100

    Modified the spark main file

--- spark_main.py
+++ spark_main.py
@@ -1,6 +1,9 @@
 # Path: spark_main.py
 
 # Import the required modules
+from spark_solution.spark_session import SparkContextManager
+from spark_solution.spark_warehouse import DataWarehouse
+from spark_solution.spark_analysis import SparkQueryExecutor
 from spark_solution.spark_extract_data import extract_raw_data
 from spark_solution.spark_preprocess_data import SparkDataPreprocessor
 
@@ -12,7 +15,20 @@ def main():
 
     # Preprocess raw data
     preprocessor = SparkDataPreprocessor(raw_df)
-    processed_df = preprocessor.preprocess_raw_data()
+    processed_df = preprocessor.final_processed_data()
+    # processed_df.printSchema()
+
+    # Create the Warehouse
+    warehouse = DataWarehouse()
+    warehouse.create_all_tables()
+    warehouse.create_dec_monthly_cps_table(processed_df)
+
+    # Analysis
+    query = SparkQueryExecutor()
+    query.query_executor()
+
+    # Close the spark session
+    SparkContextManager().stop_spark_session()
 
 
 if __name__ == '__main__':
